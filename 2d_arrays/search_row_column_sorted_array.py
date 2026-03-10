c7bdf5d45b744b71c01608ccf732594103eccf08
Morningstar618 <ayush618officer@gmail.com>
2026-03-10 12:17:05 +0530

    search-2d-column-row-sorted-array

---
+++ 2d_arrays/search_row_column_sorted_array.py
@@ -0,0 +1,44 @@
+"""
+Search in a row and column sorted 2d array
+
+i/p: [
+    [10, 20, 30, 40],
+    [15, 25, 35, 45],
+    [27, 29, 37, 48],
+    [32, 33, 39, 50]
+], k = 29
+
+o/p: 2 1
+"""
+
+def search(arr: list, x: int):
+    """Search in a row/column sorted 2d array"""
+
+    row = len(arr)
+    col = len(arr[0])
+
+    i = 0
+    j = col - 1
+
+    while (i < row and j >= 0):
+        if arr[i][j] == x:
+            print(f"{x} found at index [{i}][{j}]")
+            return
+
+        if arr[i][j] > x:
+            j -= 1
+        else:
+            i += 1
+
+    print(f"{x} not found")
+
+
+if __name__ == '__main__':
+    nums = [
+        [10, 20, 30, 40],
+        [15, 25, 35, 45],
+        [27, 29, 37, 48],
+        [32, 33, 39, 50]
+    ]
+
+    search(nums, 29) # 29 found at index [2][1]
